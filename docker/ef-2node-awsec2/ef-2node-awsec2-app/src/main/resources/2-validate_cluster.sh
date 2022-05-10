3751af105d32582dee65e71b1a5e716bce74c0d3
Robert Krajewski <rkrajews@tibco.com>
2022-05-10 15:57:00 -0400

    SB-51896: Remove docker/ef-2node-awsec2 until it is ready.

--- docker/ef-2node-awsec2/ef-2node-awsec2-app/src/main/resources/2-validate_cluster.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-## Execute this script to validate cluster
-
-echo "Run epadmin on Node A"
-docker exec A.${project.artifactId} epadmin --servicename=${project.artifactId} display cluster
-
-echo "Run epadmin on Node B"
-docker exec B.${project.artifactId} epadmin --servicename=${project.artifactId} display cluster
-
